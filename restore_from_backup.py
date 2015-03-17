25c751619eabf57a6a50d12edb77788283682a2a
Ubuntu <ubuntu@ip-172-31-31-77.us-west-1.compute.internal>
2015-03-17 22:08:42 +0000

    add restore_from_backup.py

---
+++ restore_from_backup.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python
+
+import sys
+
+try:
+	dbname,backup_file = sys.argv[1], sys.argv[2]
+except:
+	print 'Usage: odoo_restore <new_dbname> <backup_file>\n'
+	sys.exit(1)
+
+metadata = dict (
+	CREATEDB_CMD = 'createdb -U odoo -h localhost %s',
+	RESTORE_CMD = 'psql -U odoo -h localhost -d %s -f %s'
+)
+
+import subprocess as sp
+
+
+def fake(f):
+	def nf(*a, **ka):
+		print('Fake invoke: %(command)s'%locals()['ka'])
+	return nf
+
+#@fake
+def issue(command=None):
+	if command:
+		sp.check_call(command.split())
+
+issue(command = metadata['CREATEDB_CMD']%dbname)
+issue(command = metadata['RESTORE_CMD']%(dbname, backup_file))
+
+
